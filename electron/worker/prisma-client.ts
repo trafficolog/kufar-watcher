6e1d1000e92bd666f91bdeab7c031629a3f5969b
Trafficolog <55046189+trafficolog@users.noreply.github.com>
2026-09-13 10:12:05 +0300

    feat: retain prisma connection source for monitor leases

--- electron/worker/prisma-client.ts
+++ electron/worker/prisma-client.ts
@@ -2,10 +2,20 @@ import { PrismaPg } from '@prisma/adapter-pg'
 
 import { PrismaClient } from '../../generated/prisma/client'
 
+const connectionStrings = new WeakMap<PrismaClient, string>()
+
 export function createPrismaClient(connectionString = process.env.DATABASE_URL): PrismaClient {
   if (!connectionString) {
     throw new Error('DATABASE_URL is required to create PrismaClient')
   }
 
-  return new PrismaClient({ adapter: new PrismaPg({ connectionString }) })
+  const prisma = new PrismaClient({
+    adapter: new PrismaPg({ connectionString }),
+  })
+  connectionStrings.set(prisma, connectionString)
+  return prisma
+}
+
+export function getPrismaClientConnectionString(prisma: PrismaClient): string | undefined {
+  return connectionStrings.get(prisma)
 }
