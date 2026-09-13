2808475af20b37e49eb458759852014368fd085c
Trafficolog <55046189+trafficolog@users.noreply.github.com>
2026-09-13 15:18:12 +0300

    feat: bridge safe Telegram desktop state

--- electron/preload/desktop-api.ts
+++ electron/preload/desktop-api.ts
@@ -1,6 +1,7 @@
 import { IPC, type BootState, type KufarDesktopApi } from '../../shared/ipc'
+import type { TelegramDesktopState } from '../../shared/telegram'
 
-type RendererListener = (event: unknown, state: BootState) => void
+type RendererListener = (event: unknown, payload: unknown) => void
 
 export interface IpcRendererLike {
   invoke(channel: string): Promise<unknown>
@@ -24,10 +25,28 @@ export function createDesktopApi(ipcRenderer: IpcRendererLike): KufarDesktopApi 
         await ipcRenderer.invoke(IPC.appExit)
       },
       onBootState(listener): () => void {
-        const handleBootState: RendererListener = (_event, state) => listener(state)
+        const handleBootState: RendererListener = (_event, state) => listener(state as BootState)
         ipcRenderer.on(IPC.bootEvent, handleBootState)
         return () => {
           ipcRenderer.removeListener(IPC.bootEvent, handleBootState)
+        }
+      },
+    },
+    telegram: {
+      async getState() {
+        return (await ipcRenderer.invoke(IPC.telegramStateGet)) as TelegramDesktopState
+      },
+      async bindCandidate() {
+        return (await ipcRenderer.invoke(IPC.telegramBindCandidate)) as Awaited<
+          ReturnType<KufarDesktopApi['telegram']['bindCandidate']>
+        >
+      },
+      onState(listener): () => void {
+        const handleState: RendererListener = (_event, state) =>
+          listener(state as TelegramDesktopState)
+        ipcRenderer.on(IPC.telegramStateEvent, handleState)
+        return () => {
+          ipcRenderer.removeListener(IPC.telegramStateEvent, handleState)
         }
       },
     },
